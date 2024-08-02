b20bd324922bba2b28541f71e2c9ff9798b82581
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2024-08-02 10:46:28 +0530

    social links

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React from 'react';
 import Link from 'next/link';
 
+import { FaFacebook, FaInstagram } from "react-icons/fa6";
+
 const FootLinks = [
     {
         no: 1,
@@ -59,11 +61,11 @@ const Footer = () => {
     return (
         <section className="md:px-[112px] bg-[#2E4149] py-20 px-4 md:px-0">
             <footer className="flex flex-col items-center justify-center space-y-16">
-                <div className="w-full flex flex-col md:flex-row md:justify-evenly space-y-4 md:space-y-0 p-4 md:p-0">
+                <div className="w-full flex flex-col md:flex-row md:justify-between space-y-4 md:space-y-0 p-4 md:p-0">
                     <div>
                         <Image src='/svg/whitelogo.svg' alt='LOGO' width={0} height={0} className="w-[100px] h-[100px]" />
                     </div>
-                    <div className="space-y-3">
+                    {/* <div className="space-y-3">
                         <h5>BOOK A TOUR</h5>
                         <ul>
                             <li>MONDAY TO SATURDAY</li>
@@ -83,6 +85,14 @@ const Footer = () => {
                             <li>Lorem ipsum dolor sit amet,</li>
                             <li>Whitefield, Bangalore XXXXXX</li>
                         </ul>
+                    </div> */}
+                    <div className="flex items-end gap-6">
+                        <Link href='https://facebook.com/tgccindia' target="_blank" rel="noopener noreferrer">
+                            <FaFacebook size={24} />
+                        </Link>
+                        <Link href='https://instagram.com/tgccindia' target="_blank" rel="noopener noreferrer">
+                            <FaInstagram size={24} />
+                        </Link>
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-center text-center">
