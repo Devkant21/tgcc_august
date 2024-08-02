65be11c1f05f19fb26407d603b5394bd4a35ca89
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2024-08-02 16:13:15 +0530

    academy

--- components/Home/Brand/Brand.tsx
+++ components/Home/Brand/Brand.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Brand = () => {
     return (
-        <section id='brands' className="md:px-[112px] py-40">
+        <section id='brands' className="md:px-[112px] py-20 md:py-40">
             <div className="relative w-full h-[500px]">
                 <Image src='/image/brand.webp' alt='BRAND' layout="fill" objectFit="cover" objectPosition="bottom" className="w-full h-full" />
                 <div className="absolute bottom-0 left-0 md:bottom-20 md:left-20 p-6">
--- components/Home/Brand/Carousel.tsx
+++ components/Home/Brand/Carousel.tsx
@@ -5,9 +5,9 @@ import { useState } from "react";
 import Image from "next/image";
 
 const images = [
-    { src: "/image/C1.webp", alt: "Image 1", title: "ACADEMY", desc: "The Good Craft Co Academy offers an immersive experience into the world of Indian craft spirits, where visitors can learn about artisanal distilling techniques, and the rich heritage of local ingredients. Attendees will gain hands-on knowledge of crafting premium spirits and an appreciation for the artistry and innovation that define the craft spirits industry. This academy is a must-visit for anyone passionate about fine beverages and eager to deepen their understanding of the craft behind exceptional spirits." },
-    { src: "/image/C2.webp", alt: "Image 2", title: "LAB", desc: "The Good Craft Co Lab offers a unique glimpse into the science of spirit-making, allowing visitors to explore the intricate processes behind crafting exceptional spirits. Guests will experience the world of rare spirits and Indian cocktails, gaining insights into flavor profiles, distillation techniques, and the innovation driving the craft spirits industry." },
-    { src: "/image/C3.webp", alt: "Image 3", title: "FERMENTARY", desc: "The Good Craft Co Fermentary showcases natural fermentation techniques, pushing the boundaries of flavors in craft spirits. Visitors can witness the transformative power of fermentation and learn how innovative methods create unique and complex tastes. This is a must-visit for those interested in the art and science behind exceptional flavor development in craft beverages." },
+    { src: "/image/C1.webp", alt: "Image 1", title: "ACADEMY", desc: "The Good Craft Co Academy offers an immersive experience into the world of Indian craft spirits, where visitors can learn about artisanal distilling techniques, and the rich heritage of local ingredients. Attendees will gain hands-on knowledge of crafting premium spirits and an appreciation for the artistry and innovation that define the craft spirits industry. This academy is a must-visit for anyone passionate about fine beverages and eager to deepen their understanding of the craft behind exceptional spirits.", mdesc: "Immerse in Indian craft spirits at The Good Craft Co Academy. Learn artisanal distilling techniques and the heritage of local ingredients." },
+    { src: "/image/C2.webp", alt: "Image 2", title: "LAB", desc: "The Good Craft Co Lab offers a unique glimpse into the science of spirit-making, allowing visitors to explore the intricate processes behind crafting exceptional spirits. Guests will experience the world of rare spirits and Indian cocktails, gaining insights into flavor profiles, distillation techniques, and the innovation driving the craft spirits industry.", mdesc: "Explore the science of spirit-making at The Good Craft Co Lab. Discover flavor profiles, distillation techniques, and industry innovations." },
+    { src: "/image/C3.webp", alt: "Image 3", title: "FERMENTARY", desc: "The Good Craft Co Fermentary showcases natural fermentation techniques, pushing the boundaries of flavors in craft spirits. Visitors can witness the transformative power of fermentation and learn how innovative methods create unique and complex tastes. This is a must-visit for those interested in the art and science behind exceptional flavor development in craft beverages.", mdesc: "Experience natural fermentation at The Good Craft Co Fermentary. See how innovative methods create unique and complex flavors in craft beverages." },
 ];
 
 const BrandCarousel = () => {
@@ -35,16 +35,17 @@ const BrandCarousel = () => {
     };
 
     return (
-        <section className="py-40">
+        <section className="py-10 md:py-40">
             <div className="">
-                <div className="block md:hidden w-full pb-4">
+                {/* <div className="block md:hidden w-full pb-4">
                     <div className="flex flex-col">
                         <div className="bg-[#416E80] p-4 flex items-center justify-center">{images[currentIndex].title}</div>
                         <div className="bg-[#E9E2CF] text-[#416E80] p-4">
-                            <p>{images[currentIndex].desc}</p>
+                            <p className="hidden md:block">{images[currentIndex].desc}</p>
+                            <p className="block md:hidden">{images[currentIndex].mdesc}</p>
                         </div>
                     </div>
-                </div>
+                </div> */}
                 <div className="md:pl-[56px] flex items-center gap-4">
                     <button onClick={handlePrev} aria-label="Previous Image" className="hidden md:block">
                         <Image
@@ -70,11 +71,12 @@ const BrandCarousel = () => {
                                 />
                             </div>
                         ))}
-                        <div className="hidden md:block absolute top-1/2 right-0 transform -translate-y-20 max-w-[600px] h-auto opacity-85">
+                        <div className="absolute top-1/2 right-0 transform md:-translate-y-20 max-w-[600px] h-auto opacity-85">
                             <div className="flex flex-col">
                                 <div className="bg-[#416E80] p-4 flex items-center justify-center">{images[currentIndex].title}</div>
                                 <div className="bg-[#E9E2CF] text-[#416E80] p-4">
-                                    <p>{images[currentIndex].desc}</p>
+                                    <p className="hidden md:block">{images[currentIndex].desc}</p>
+                                    <p className="block md:hidden">{images[currentIndex].mdesc}</p>
                                 </div>
                             </div>
                         </div>
